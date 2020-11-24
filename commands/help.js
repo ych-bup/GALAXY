e878be35cbd0c112410c179b30c1ce3b159eec09
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-24 11:34:22 +0900

    d

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ exports.run = async(client, message, args) => {
             { name: 'infoHelp', value: 'Info Command Help', inline: true },
             { name: 'moderateHelp', value: 'Moderation Command Help', inline: true }
         )
-        .setThumbnail(message.guild.me.displayAvatarURL())
+        .setThumbnail(`https://images-ext-1.discordapp.net/external/Vk8OwWQbmZ9T7PWSvc7l4Z6IQ0KCuFJsgoCFHqGIfgU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/780408136460402738/be0ac2bc8b228bac9fc21e82740e2c30.png?width=601&height=601`)
         .setFooter(`${message.author.username}`,`${message.author.displayAvatarURL()}`)
         .setTimestamp()
     message.channel.send(originalEM);
--- commands/prefix.js
+++ commands/prefix.js
@@ -6,4 +6,20 @@ exports.run = async(client, message, args) => {
 
     if(!newPrefix) return message.channel.send('Current prefix is \`\`' + prefix + '\`\`');
     if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(`:x: You don't have permission!`)
+
+    const test = await guildprefix.findOne({ serverid: message.guild.id });
+
+    if(test == null) {
+        await new guildprefix({ serverid: message.guild.id, prefix: newPrefix }).save();
+    }
+    else {
+        await guildprefix.opdateOne({ serverid: message.guild.id }, { prefix: newPrefix });
+    }
+
+    const setPrefix = await guildprefix.findOne({ serverid: message.guild.id })
+
+    const embedPF = new Discord.MessageEmbed()
+        .setColor('#0067A3')
+        .setDescription('Successfully set prefix => \`\`'+setPrefix.prefix+'\`\`')
+    await message.channel.send(embedPF);
 }
