1af302b683db22c6c7745f973d6d1877d070e851
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-24 11:38:06 +0900

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = async(client, message, args) => {
     const originalEM = new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle('')
+        .setAuthor(`Ich.BOT Plugins Commands`,`https://images-ext-1.discordapp.net/external/Vk8OwWQbmZ9T7PWSvc7l4Z6IQ0KCuFJsgoCFHqGIfgU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/780408136460402738/be0ac2bc8b228bac9fc21e82740e2c30.png?width=601&height=601`)
         .addFields(
             { name: 'textHelp', value: 'Text Command Help', inline: true },
             { name: 'infoHelp', value: 'Info Command Help', inline: true },
