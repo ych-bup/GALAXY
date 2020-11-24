43ef07f5bb1703327ed8d7ea2ff3eb78c5af9c16
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-24 22:39:25 +0900

    Update blood-pressure.js

--- commands/blood-pressure.js
+++ commands/blood-pressure.js
@@ -4,13 +4,28 @@ const mongoose = require('mongoose');
 exports.run = async(client, message, args) => {
     const userDATA = mongoose.model('userDATA', new mongoose.Schema({
         serverid: String,
-        highPressure: Number,
         lowPressure: Number,
         userName: String
     }));
 
-    const args1 = message.content.split(" ").slice(1);
-    const args2 = message.content.split(" ").slice(2);
+    const args1 = message.content.split(" ").slice(2);
 
-    message.channel.send(`${args1}\n${args2}`);
+
+    let userdata = await userDATA.find({ serverid: message.guild.id })
+
+    userdata = userdate.map(element => element.lowPressure)
+
+    try{
+        await new userDATA({ serverid: message.guild.id, lowPressure: args1, userName: message.author.username }).save();
+    
+        
+    }
+    catch(err) {
+        console.log(err);
+        message.channel.send(':x: Something wrong.');
+    }
+
+    userdata.forEach(element => {
+        message.channel.send(element);
+    })
 }
