bf0a70278ecb2d0f30884dfeb4e403f7a0714874
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-24 22:26:40 +0900

    Update blood-pressure.js

--- commands/blood-pressure.js
+++ commands/blood-pressure.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
 const mongoose = require('mongoose');
-const client1 = new Discord.Client();
 
 exports.run = async(client, message, args) => {
-    client1.on('message', async(msg) => {
-        msg.channel.send('Success!');
-    })
+    const userDATA = mongoose.model('userDATA', new mongoose.Schema({
+        serverid: String,
+        highPressure: Number,
+        lowPressure: Number,
+        userName: String
+    }));
+
+    const args1 = message.content.split(" ").slice(1);
+    const args2 = message.content.split(" ").slice(2);
+
+    message.channel.send(`${args1}\n${args2}`);
 }
