384a8e00e3729f0bcdbe1492b9bf905759356007
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-27 22:21:54 +0900

    dd

--- commands/texthelp.js
+++ commands/texthelp.js
@@ -4,10 +4,11 @@ exports.run = async(client, message, args) => {
     const textEM = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('**Text Command Help**')
-        .addField(prefix+'help', 'help about this bot.')
-        .addField(prefix+'ang', 'Gimothi!')
-        .addField(prefix+'ping', 'pong!')
-        .addField(prefix+'calc', 'Calculation.')
-        .addField(prefix+'search', 'search on WolframAlpha.')
+        .addField('**help**', 'help about this bot.')
+        .addField('**ang**', 'Gimothi!')
+        .addField('**ping**', 'pong!')
+        .addField('**calc**', 'Calculation.')
+        .addField('**search**', 'search on WolframAlpha.')
+        .setFooter('Guild Prefix is '+'\`\`'+prefix+'\`\`')
     message.channel.send(textEM);
 }
