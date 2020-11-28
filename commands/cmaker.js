2327305ce311870b9df2e8095bb01176907e19a5
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-28 11:20:56 +0900

    Update cmaker.js

--- commands/cmaker.js
+++ commands/cmaker.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const { max } = require('moment');
 
 exports.run = async(client, message, args) => {
-    const user = message.mention.users.first();
+    const user = message.mention.members.first();
 
-    const percentage = Math.floor(Math.random() * (100-0+1) + 0);
+    const percentage = Math.floor(Math.random() * (100));
 
     message.channel.send(`**${percentage}%** of **${message.author.username}** loves **${user.username}** 💕~`)
 }
