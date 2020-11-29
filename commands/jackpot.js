cf2fd4f78ad017138668541d35724b2a88b439ed
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-29 19:20:24 +0900

    Update jackpot.js

--- commands/jackpot.js
+++ commands/jackpot.js
@@ -5,11 +5,11 @@ exports.run = async(client, message, args) => {
     const message2 = ["7"];
     const message3 = ["7"];
     
-        if(someMessage == someMessage1 == someMessage2 == "7"){
+        if(message1 == message2 == message3 == "7"){
             const JackPot1 = new Discord.MessageEmbed()
                 .setColor('RANDOM')
                 .setTitle('**JACKPOT**')
-                .setDescription('\`\`' + someMessage + '\`\` ' + '\`\`' + someMessage1 + '\`\` ' + '\`\`' + someMessage2 + '\`\`')
+                .setDescription('\`\`' + message1+ '\`\` ' + '\`\`' + message2 + '\`\` ' + '\`\`' + message3 + '\`\`')
                 .addField('JACK POT!!!!','Have a good Day!!!')
                 .addField('OHHHHHH!','You bit about **0.42%**!!')
                 .setThumbnail('https://image.freepik.com/free-vector/big-win-777-lottery-vector-casino-concept-with-slot-machine-win-jackpot-game-slot-machine-illust_53562-4198.jpg')
@@ -20,7 +20,7 @@ exports.run = async(client, message, args) => {
             const JackPot = new Discord.MessageEmbed()
                 .setColor('RANDOM')
                 .setTitle('**JACKPOT**')
-                .setDescription('\`\`' + someMessage + '\`\` ' + '\`\`' + someMessage1 + '\`\` ' + '\`\`' + someMessage2 + '\`\`')
+                .setDescription('\`\`' + message1 + '\`\` ' + '\`\`' + message2 + '\`\` ' + '\`\`' + message3 + '\`\`')
                 .addField('Good Luck!','Have a good Day!')
                 .setFooter(`${message.author.tag}`,`${message.author.displayAvatarURL()}`)
             return message.channel.send(JackPot);
