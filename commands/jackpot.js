3db8b6f3fcbe1b3fb6b99b9606189ad3f77dd8fd
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-11-29 19:23:51 +0900

    Update jackpot.js

--- commands/jackpot.js
+++ commands/jackpot.js
@@ -4,25 +4,27 @@ exports.run = async(client, message, args) => {
     const message1 = ["7"];
     const message2 = ["7"];
     const message3 = ["7"];
+    const JackPot1 = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('**JACKPOT**')
+        .setDescription('\`\`' + message1+ '\`\` ' + '\`\`' + message2 + '\`\` ' + '\`\`' + message3 + '\`\`')
+        .addField('JACK POT!!!!','Have a good Day!!!')
+        .addField('OHHHHHH!','You bit about **0.42%**!!')
+        .setThumbnail('https://image.freepik.com/free-vector/big-win-777-lottery-vector-casino-concept-with-slot-machine-win-jackpot-game-slot-machine-illust_53562-4198.jpg')
+        .setFooter(`${message.author.tag}`,`${message.author.displayAvatarURL()}`)    
     
-        if(message1 == message2 == message3 == "7"){
-            const JackPot1 = new Discord.MessageEmbed()
-                .setColor('RANDOM')
-                .setTitle('**JACKPOT**')
-                .setDescription('\`\`' + message1+ '\`\` ' + '\`\`' + message2 + '\`\` ' + '\`\`' + message3 + '\`\`')
-                .addField('JACK POT!!!!','Have a good Day!!!')
-                .addField('OHHHHHH!','You bit about **0.42%**!!')
-                .setThumbnail('https://image.freepik.com/free-vector/big-win-777-lottery-vector-casino-concept-with-slot-machine-win-jackpot-game-slot-machine-illust_53562-4198.jpg')
-                .setFooter(`${message.author.tag}`,`${message.author.displayAvatarURL()}`)
-            return message.channel.send(JackPot1);
-        }
-        else{
-            const JackPot = new Discord.MessageEmbed()
-                .setColor('RANDOM')
-                .setTitle('**JACKPOT**')
-                .setDescription('\`\`' + message1 + '\`\` ' + '\`\`' + message2 + '\`\` ' + '\`\`' + message3 + '\`\`')
-                .addField('Good Luck!','Have a good Day!')
-                .setFooter(`${message.author.tag}`,`${message.author.displayAvatarURL()}`)
-            return message.channel.send(JackPot);
-        }
+    const JackPot = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('**JACKPOT**')
+        .setDescription('\`\`' + message1 + '\`\` ' + '\`\`' + message2 + '\`\` ' + '\`\`' + message3 + '\`\`')
+        .addField('Good Luck!','Have a good Day!')
+        .setFooter(`${message.author.tag}`,`${message.author.displayAvatarURL()}`)
+
+        
+    if(message1 == message2 == message3 == "7"){
+        return message.channel.send(JackPot1);
+    }
+    else{
+        return message.channel.send(JackPot);
+    }
 }
