f2d1202b6f3ad0c0d8632148b9d89b15302c1205
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-03 19:12:45 +0900

    Update warnuser.js

--- commands/warnuser.js
+++ commands/warnuser.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
 
 exports.run = async(client, message, args) => {
-    message.channel.send(warnuser.join(' || '))
+    message.channel.send(warnuser.join(" || ") ? warnuser.join(" || ") : "경고받은 사람 없음")
 }
