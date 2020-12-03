fadb96f5ee8897c805a19f86f20cff13e99806a8
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-03 19:37:59 +0900

    Delete warnuser.js

--- commands/warnuser.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = async(client, message, args) => {
-    const user = message.mentions.members.first();
-    warnuser = warnuser.map(element => `**userid** : ${user.id}`)
-
-    message.channel.send(warnuser[0]);
-}
