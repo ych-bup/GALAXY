1423e75056e113bd15fabfe807ce39549c890881
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-03 22:02:00 +0900

    Update deletewarn.js

--- commands/deletewarn.js
+++ commands/deletewarn.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 
 exports.run = async(client, message, args) => {
-    const fuck = message.mentions.members.first();
-    let e = await warnuser.findOne({ serverid: message.guild.id, userid: fuck.user.id })
+    const user = message.mentions.members.first();
+    let e = await warnuser.findOne({ serverid: message.guild.id, userid: user.id })
     if(!e) return message.channel.send('Don\'t exist.');
 
     try{
-        warnuser.find({userid:fuck.user.id}).deleteMany().exec()
+        warnuser.find({userid:user.id}).deleteMany().exec()
         message.channel.send('Success.')
     }
     catch(err) {
