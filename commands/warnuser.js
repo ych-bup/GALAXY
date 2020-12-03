1252b42c297d210d5d326db672e947401a1b9cf8
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-03 21:54:30 +0900

    Update warnuser.js

--- commands/warnuser.js
+++ commands/warnuser.js
@@ -5,5 +5,5 @@ exports.run = async(client, message, args) => {
 
     modlogs = modlogs.map(element => element.userid)
 
-    message.channel.send(`${modlogs.join(" | ").substring(5) ? modlogs.join(" | ").substring(5) : "No user."}`);
+    message.channel.send(`${modlogs.join(" | ").substring(21) ? modlogs.join(" | ").substring(21) : "No user."}`);
 }
