cd91ad87dfb62022fa5b6f6358bbb3fb355c04fb
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-05 11:20:45 +0900

    Update warnuser.js

--- commands/warnuser.js
+++ commands/warnuser.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = async(client, message, args) => {  
     let modlogs = await warnuser.find({ serverid: message.guild.id })
 
-    modlogs = modlogs.map(element => element.usertag)
+    modlogs = modlogs.map(element => element.userid)
 
     message.channel.send(`${modlogs.join(" | ").substring(21) ? modlogs.join(" | ").substring(21) : "No user."}`);
 }
