d14611c5e4aa811b33dd5aac8971ce6cd887c167
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-12-15 21:04:49 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ exports.run = async(client, message, args) => {
     const yourping = new Date().getTime() - message.createdTimestamp;
     const embed = new Discord.MessageEmbed()
         .setColor('#8b00ff')
-        .setDescription(`🏓 Ping: ${client.ws.ping}ms / Your ping: ${Math.abs(yourping)}ms`)
+        .setDescription(`🏓 Ping: ${client.ws.ping}ms / Latency: ${Math.abs(yourping)}ms`)
     message.channel.send(embed)
 }
 
