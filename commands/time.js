53cc351acf12ca97caea16a11df3e86ffe135be2
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-03-30 12:47:35 +0900

    d

--- commands/time.js
+++ commands/time.js
@@ -1,11 +1,16 @@
 const Discord = require('discord.js');
 
-exports.run = async(client,message,args) => {
-    var newDate = new Date();
+
+exports.run = async (client, message, args) => {
+    var moment = require('moment');
+
+    require('moment-timezone');
+    moment.tz.setDefault("Asia/Seoul");
+    
     const embed = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('Now it is...')
-        .setDescription(newDate)
-    
+        .setDescription(moment().format('YYYY-MM-DD HH:mm:ss'))
+
     message.channel.send(embed);
 }
