1091d4b199b3c9d0a759fe10a7f33ceaaec13409
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-05-07 19:40:02 +0900

    s

--- commands/hangang.js
+++ commands/hangang.js
@@ -1,9 +1,25 @@
 const Discord = require("discord.js");
+const request = require('request');
 
 exports.run = async(client, message, args) => {
 
-    const obj = JSON.parse("https://api.hangang.msub.kr");
+    var hangang_url = "https://api.hangang.msub.kr";
+    var hangang_temp;
+    var hangang_time;
+    var hangang_station;
 
-    message.channel.send(obj.temp);
+    request({
+        url: hangang_url,
+        json: true,
+    }, function (err,res, body) {
+        if(!err && res.statusCode === 200) {
+            var obj = JSON.parse(body);
+            hangang_temp = obj.temp;
+            hangang_time = obj.time;
+            hangang_station = obj.station;
+        }
+    })
+
+    message.channel.send(hangang_temp);
 
 }
