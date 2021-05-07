75c47d356046df8072d98fb84c628191e019f7ff
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-05-07 20:16:43 +0900

    Update hangang.js

--- commands/hangang.js
+++ commands/hangang.js
@@ -4,14 +4,20 @@ const request = require('request');
 exports.run = async(client, message, args) => {
 
     var hangang_url = "https://api.hangang.msub.kr";
-    var hangang_temp;
-    var hangang_time;
-    var hangang_station;
 
     request(hangang_url, function(error,response,body) {
         if(!error && response.statusCode == 200) {
             var obj = JSON.parse(body);
-            message.channel.send(obj.temp);
+
+            const hangang_embed = new Discord.MessageEmbed()
+                .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL()}`)
+                .setColor('RANDOM')
+                .setTitle(`${obj.temp}℃`)
+                .setDescription(`Observed on **${obj.station}** at **${obj.time}**`)
+                .setThumbnail('https://cdn.blog.ivlis.kr/data/other-services/hangang.ivlis.kr-bg.jpg')
+                .setFooter('힘내요!!')
+
+            message.channel.send(hangang_embed);
         }
     })
 
