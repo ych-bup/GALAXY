1629c890587121f2bf652b75310a11a1b6a65865
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-05-07 19:25:14 +0900

    Update hangang.js

--- commands/hangang.js
+++ commands/hangang.js
@@ -1,12 +1,8 @@
 const Discord = require("discord.js");
 
 exports.run = async(client, message, args) => {
-    
-    var requestURL = 'https://api.hangang.msub.kr/'
 
-    
-
-    const obj = JSON.parse(requestURL);
+    const obj = JSON.parse("https://api.hangang.msub.kr");
 
     message.channel.send(obj.temp);
 
