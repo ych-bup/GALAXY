e20e960c7a737b5c721df1cd0e8b0b7b062dbdcd
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-05-07 20:18:27 +0900

    Update hangang.js

--- commands/hangang.js
+++ commands/hangang.js
@@ -12,7 +12,7 @@ exports.run = async(client, message, args) => {
             const hangang_embed = new Discord.MessageEmbed()
                 .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL()}`)
                 .setColor('RANDOM')
-                .setTitle(`${obj.temp}℃`)
+                .setTitle(`***${obj.temp}℃***`)
                 .setDescription(`Observed on **${obj.station}** at **${obj.time}**`)
                 .setThumbnail('https://cdn.blog.ivlis.kr/data/other-services/hangang.ivlis.kr-bg.jpg')
                 .setFooter('힘내요!!')
