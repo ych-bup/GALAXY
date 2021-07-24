84ee3a7a258ffbf2c7297600394c2ed3505cf2c9
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-07-24 22:48:24 +0900

    d

---
+++ commands/확진자.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+exports.run = async(client, message, args) => {
+    const getHtml = async () => {
+        try {
+            return await axios.get("http://ncov.mohw.go.kr/");
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    getHtml()
+        .then(html => {
+            let ulList = [];
+            const $ = cheerio.load(html.data);
+            const $bodylist = $("div.wrap.nj div.mainlive_container div.container div div.liveboard_layout div.liveNum_today_new div.datalist ul").children("li");
+
+            $bodylist.each(function(i, elem) {
+                ulList[i] = {
+                    title: $(this).find('span.data').text()
+                };
+            });
+
+            const data = ulList.filter(n=>n.title);
+            return data;
+        })
+        .then(res => console.log(res));
+}
