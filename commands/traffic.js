187ada8ed610bf6ed6f808d102d5be080447f068
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-08-27 19:35:14 +0900

    Update traffic.js

--- commands/traffic.js
+++ commands/traffic.js
@@ -1,18 +1,25 @@
 const Discord = require('discord.js');
 
 exports.run = async(client, message, args) => {
-    if(message.author.hasPermission == 'ADMINISTRATOR') return message.channel.send(":x: You don't have permision!")
-
     const args1 = message.content.split(" ").slice(1);
+    const channelname = message.guild.channels.cache.find(ch => ch.name === args1);
+    
+    if(!args1) return message.channel.send(":x: You don't enter channel name!");
+    if(!channelname) return message.channel.send(":x: Channel doesn't exist!");
+    if(!message.author.hasPermission == 'ADMINISTRATOR') return message.channel.send(":x: You don't have permision!")
 
-    if(!message.guild.channels.cache.find(ch => ch.name === args1)) return message.channel.send(":x: Channel doesn't exist!");
-
-    try{
-        trafficChannel = args1;
-        message.channel.send(`:white_check_mark: Traffic channel setup successful! channel-`);
-    } catch(err) {
-        message.channel.send(`***ERROR***\n\n\`\`\`${err}\`\`\``);
+    const test = await traffic.findOne({ serverid: message.guild.id });
+    
+    if(test==null){
+        await new traffic({ serverid: message.guild.id, channelName: args1 }).save();
     }
+    else {
+        await new traffic.updateOne({ channelName: args1});
+    }
+
+    await message.channel.send(`:white_check_mark: Traffic channel setup successful! channel-#${test.channelName}`);
+
+
 
 
 }
