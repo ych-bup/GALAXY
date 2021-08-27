ec9ecb6d7424e278fd0fa583ad40f94f7d871e69
RythmAviation <67253200+ych-bup@users.noreply.github.com>
2021-08-28 08:48:08 +0900

    Update traffic.js

--- commands/traffic.js
+++ commands/traffic.js
@@ -3,9 +3,8 @@ const Discord = require('discord.js');
 exports.run = async(client, message, args) => {
     const args1 = message.content.split(" ").slice(1).join(" ");
     const channelname = message.guild.channels.cache.find(ch => ch.name === args1);
-    
     if(!args1) return message.channel.send(":x: You don't enter channel name!");
-    if(!channelname) return message.channel.send(":x: Channel doesn't exist!");
+    if(!message.channel.name.includes(args1)) return message.channel.send(":x: Channel doesn't exist!");
     if(!message.author.hasPermission == 'ADMINISTRATOR') return message.channel.send(":x: You don't have permision!")
 
     const test = await traffic.findOne({ serverid: message.guild.id });
